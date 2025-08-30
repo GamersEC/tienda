9242e5631f2cf40c7ee5291b8d2b49273afc92f6
marcus <marcusalexander1999@gmail.com>
2025-08-30 12:03:06 -0500

    carpeta migraciones

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# Salir inmediatamente si un comando falla
 set -e
 
 echo "Aplicando migraciones de la base de datos..."
@@ -10,6 +9,5 @@ echo "Verificando/Creando usuario administrador por defecto..."
 flask crear-admin-auto
 
 echo "Iniciando el servidor Gunicorn..."
-# exec "$@" permite que el comando que pones en el Dockerfile (CMD) se ejecute
-# y se convierta en el proceso principal del contenedor.
+
 exec "$@"
